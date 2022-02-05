312e6446e26ffb69570b01149a707cd9bff773f5
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2022-02-05 19:57:22 +0200

    Updated CRA template to React 18

--- MoveShowcaseDDD/ClientApp/src/index.tsx
+++ MoveShowcaseDDD/ClientApp/src/index.tsx
@@ -4,11 +4,12 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLDivElement;
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
