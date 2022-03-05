15d8a8164b267d3861f679d442f2040b709c2204
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2022-03-05 14:18:39 +0200

    Exception handling on auth service

--- MoveShowcaseSPA/ClientApp/src/services/authService.ts
+++ MoveShowcaseSPA/ClientApp/src/services/authService.ts
@@ -1,19 +1,29 @@
+import { toast } from 'react-toastify';
 import http from './httpService';
 
 const apiEndpoint = `${process.env.REACT_APP_API_URL}user/`;
 
 export const login = async (email: string, password: string) => {
-  await http.post(`${apiEndpoint}Login`, { username: email, password });
+  try {
+    const response = await http.post(`${apiEndpoint}Login`, {
+      username: email,
+      password,
+    });
+    sessionStorage.setItem('auth_token', response.data.token);
+  } catch {
+    toast.error('An error ocurred trying to login');
+  }
 };
 
 export const logout = async () => {
   await http.post(`${apiEndpoint}Logout`);
+  sessionStorage.removeItem('auth_token');
 };
 
 export const getCurrentUser = async () => {
   try {
-    const user = await http.get(apiEndpoint);
-    return user;
+    const response = await http.get(apiEndpoint);
+    return response.data;
   } catch (ex) {
     return null;
   }
