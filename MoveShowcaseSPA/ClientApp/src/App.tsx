544540f1fdb2d029213e86351b645d60955457b0
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2022-03-05 14:16:37 +0200

    Added Loading component and lazy loading

--- MoveShowcaseSPA/ClientApp/src/App.tsx
+++ MoveShowcaseSPA/ClientApp/src/App.tsx
@@ -1,38 +1,36 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { lazy, Suspense, useEffect, useMemo, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
-import Movies from './components/Movies';
-import Customers from './components/Customers';
-import Rentals from './components/Rentals';
+import { ToastContainer } from 'react-toastify';
 import NotFound from './components/NotFound';
-import NavBar from './components/NavBar';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-import Logout from './components/Logout';
 import auth from './services/authService';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 import { User } from './interfaces/user';
 import UserContext from './context/userContext';
+import Loading from './components/Loading';
+
+const Movies = lazy(() => import('./components/Movies'));
+
+const Customers = lazy(() => import('./components/Customers'));
+
+const Rentals = lazy(() => import('./components/Rentals'));
+
+const NavBar = lazy(() => import('./components/NavBar'));
+
+const LoginForm = lazy(() => import('./components/LoginForm'));
+
+const RegisterForm = lazy(() => import('./components/RegisterForm'));
+
+const Logout = lazy(() => import('./components/Logout'));
 
 function App() {
   const [user, setUser] = useState<User>();
   useEffect(() => {
     (async () => {
-      const updateUser = ({ user: userResponse }: { user: User }) => {
-        setUser(userResponse);
-      };
-
       const getUser = async () => {
         const response = await auth.getCurrentUser();
-        if (!response) {
-          toast.error('🦄 Wow so easy!', {
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: false,
-          });
-        } else {
-          updateUser(response?.data);
+        if (response) {
+          setUser(response);
         }
       };
       await getUser();
@@ -47,34 +45,30 @@ function App() {
     [user],
   );
 
-  if (!user) return null;
-
   return (
-    <UserContext.Provider value={userContextProvider}>
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover
-      />
-      <NavBar />
-      <main className="container">
-        <Routes>
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/rentals" element={<Rentals />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </UserContext.Provider>
+    <Suspense fallback={<Loading />}>
+      <UserContext.Provider value={userContextProvider}>
+        <ToastContainer
+          position="bottom-left"
+          autoClose={5000}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+        />
+        <NavBar />
+        <main className="container">
+          <Routes>
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/rentals" element={<Rentals />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </UserContext.Provider>
+    </Suspense>
   );
 }
 
