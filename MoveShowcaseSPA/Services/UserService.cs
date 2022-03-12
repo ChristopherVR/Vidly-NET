b3d9b81260da45164863da6fb707624b943a71d0
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2022-03-12 18:46:00 +0200

    Resolved issue with UserService

--- MoveShowcaseSPA/Services/UserService.cs
+++ MoveShowcaseSPA/Services/UserService.cs
@@ -8,15 +8,15 @@ public class UserService : IUserService
         => _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
 
     public string GetName() =>
-        _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name) ?? throw new ArgumentNullException("Name cannot be null");
+        _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.GivenName) ?? throw new ArgumentNullException("Name cannot be null");
 
     public string GetSurname()
         => _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Surname) ?? throw new ArgumentNullException("Surname cannot be null");
 
     public int GetUserId() =>
-        int.Parse(_httpContextAccessor.HttpContext?.User.FindFirstValue("sub") ?? throw new ArgumentNullException("User Id cannot be null"));
+        int.Parse(_httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new ArgumentNullException("User Id cannot be null"));
 
     public string GetUserName() =>
-        _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new ArgumentNullException("UserName cannot be null");
+        _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name) ?? throw new ArgumentNullException("UserName cannot be null");
 }
 
