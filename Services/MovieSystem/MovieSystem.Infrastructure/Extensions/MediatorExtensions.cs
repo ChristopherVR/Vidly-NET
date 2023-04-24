0f75326d11955406d9d79f65bb5bdad44bf9fae6
Christopher van Rooyen <chrisjvrooyen@gmail.com>
2023-04-25 08:56:32 +1000

    Update MediatorExtensions.cs

--- Services/MovieSystem/MovieSystem.Infrastructure/Extensions/MediatorExtensions.cs
+++ Services/MovieSystem/MovieSystem.Infrastructure/Extensions/MediatorExtensions.cs
@@ -9,18 +9,18 @@ public static class MediatorExtensions
 {
     public static async Task DispatchDomainEventsAsync(this IMediator mediator, MovieContext ctx)
     {
-        IEnumerable<Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Entity>>? domainEntities = ctx.ChangeTracker
+        IEnumerable<Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Entity>> domainEntities = ctx.ChangeTracker
             .Entries<Entity>()
             .Where(x => x.Entity.DomainEvents?.Any() == true);
 
-        List<INotification>? domainEvents = domainEntities
+        List<INotification> domainEvents = domainEntities
             .SelectMany(x => x.Entity.DomainEvents)
             .ToList();
 
         domainEntities.ToList()
             .ForEach(entity => entity.Entity.ClearDomainEvents());
 
-        foreach (INotification? domainEvent in domainEvents)
+        foreach (INotification domainEvent in domainEvents)
         {
             await mediator.Publish(domainEvent);
         }
